b02339d3101f45a39033723844b6eea2a27fa609
Isaac Chung <isaac.chung@clarifai.com>
2023-09-28 15:21:40 +0200

    revamp

--- llm.py
+++ llm.py
@@ -6,7 +6,7 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2
 
-from constants import USER_ID, APP_ID, MODEL_ID, MODEL_VERSION_ID, PROMPTS
+from constants import USER_ID, APP_ID, MODEL_ID, MODEL_VERSION_ID, PROMPTS, TRANSLATE_USER_ID, TRANSLATE_APP_ID, TRANSLATE_MODEL_IDS
 
 class ClarifaiPrompter:
     def __init__(self):
@@ -44,9 +44,48 @@ class ClarifaiPrompter:
             if i == retries - 1:
                 print(post_model_outputs_response.status)
                 raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
-            print(f"Model prediction trial {i} failed. Sleeping for one minute.")
+            print(f"Prompt trial {i} failed. Sleeping for one minute.")
             time.sleep(60)
 
+
+class ClarifaiTranslator:
+    def __init__(self):
+        self.models = TRANSLATE_MODEL_IDS
+        self.userDataObject = resources_pb2.UserAppIDSet(user_id=TRANSLATE_USER_ID, app_id=TRANSLATE_APP_ID)
+        self.metadata = (('authorization', 'Key ' + os.environ.get("CLARIFAI_PAT_PROD")),)
+
+        channel = ClarifaiChannel.get_grpc_channel()
+        self.stub = service_pb2_grpc.V2Stub(channel)
+
+    def _predict(self, text_input, model_id=MODEL_ID, model_version_id=MODEL_VERSION_ID):
+        return self.stub.PostModelOutputs(
+            service_pb2.PostModelOutputsRequest(
+                user_app_id=self.userDataObject,
+                model_id=model_id,
+                version_id=model_version_id, 
+                inputs=[
+                    resources_pb2.Input(
+                        data=resources_pb2.Data(
+                            text=resources_pb2.Text(
+                                raw=text_input
+                            )
+                        )
+                    )
+                ]
+            ),
+            metadata=self.metadata
+        )
+        
+    def predict(self, text_input, model_id="translation-english-to-ukrainian-text", model_version_id=None, retries=3) -> str:
+        for i in range(retries):
+            post_model_outputs_response = self._predict(text_input, model_id, model_version_id)
+            if post_model_outputs_response.status.code == status_code_pb2.SUCCESS:
+                return post_model_outputs_response.outputs[0].data.text.raw
+            if i == retries - 1:
+                print(post_model_outputs_response.status)
+                raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+            print(f"Translation trial {i} failed. Sleeping for one minute. {model_id=}. {model_version_id=}")
+            time.sleep(60)
 
 def main():
     prompter = ClarifaiPrompter()
--- tweet.py
+++ tweet.py
@@ -1,8 +1,11 @@
 import os
 import tweepy
 
+from tweetcapture import TweetCapture
+import asyncio
+
 from constants import TWITTER_USER_ID
-from llm import ClarifaiPrompter
+from llm import ClarifaiPrompter, ClarifaiTranslator
 from upload import ClarifaiUploader
 
 consumer_key = os.environ.get("CONSUMER_KEY")
@@ -22,7 +25,6 @@ def clean_llm_output(raw_text: str) -> str:
         cleaned = cleaned.replace("Example:", "")
         cleaned = cleaned.replace("Here's a possible tweet:", "")
         cleaned = cleaned.replace("Tweet:", "")
-        # cleaned = raw_text.replace("\n\n", "\n")
     except:
         raise Exception("Raw output: %s, cleaned:%s" % (raw_text, str(cleaned)))
     return cleaned
@@ -31,42 +33,48 @@ def build_tweet_url(tweet_id:str) -> str:
     return "https://twitter.com/%s/status/%s" %(TWITTER_USER_ID, tweet_id)
 
 def main(generate_only=False, tweet_only=False):
+    """
+    For each prompt, generate all language variants and upload.
+    """
     prompter = ClarifaiPrompter()
-
     while True:
         output = prompter.predict()
         tweet = clean_llm_output(output)
         if len(tweet) < 280:
             break
-
-    if generate_only:
-        print(f"{tweet=}")
-        exit()
-        
+    
+    # initialize client only once.
     client = tweepy.Client(
         consumer_key=consumer_key, consumer_secret=consumer_secret,
         access_token=access_token, access_token_secret=access_token_secret
     )
+    ## https://github.com/xacnio/tweetcapture/blob/main/tweetcapture/examples/tweet_screenshot.py    
+    tc = TweetCapture()
 
-    response = client.create_tweet(
-        text=tweet
-    )
-    tweet_id = response.data['id']
-    print(f"Tweet ID: {tweet_id}")
+    translator = ClarifaiTranslator()
+    for mid, mvid in translator.models.items():
+        try:
+            translated_tweet = translator.predict(tweet, model_id=mid, model_version_id=mvid)
+        except:
+            translated_tweet = tweet #default back to English.
+        if generate_only:
+            print(f"{translated_tweet=}")
+            continue
 
-    if tweet_only:
-        exit()
+        response = client.create_tweet(
+            text=translated_tweet
+        )
+        tweet_id = response.data['id']
+        print(f"Tweet ID: {tweet_id}")
 
-    tweet_url = build_tweet_url(tweet_id)
-    image_path = f"{tweet_id}.png"
-    
-    ## https://github.com/xacnio/tweetcapture/blob/main/tweetcapture/examples/tweet_screenshot.py
-    from tweetcapture import TweetCapture
-    import asyncio
-    tweet = TweetCapture()
-    asyncio.run(tweet.screenshot(tweet_url, image_path, mode=3, night_mode=0))
+        if tweet_only:
+            exit()
 
-    ClarifaiUploader().upload(image_file_location=image_path)
+        tweet_url = build_tweet_url(tweet_id)
+        image_path = f"{tweet_id}.png"
+        
+        asyncio.run(tc.screenshot(tweet_url, image_path, mode=3, night_mode=0))
+        ClarifaiUploader().upload(image_file_location=image_path)
 
 
 if __name__ == "__main__":
